996ad694fbd6837b44b33c73928a5b76f2b91407
Alexey Ermakov <fufler@gmail.com>
2016-03-11 19:58:37 +0300

    Примеры ко второй лекции

---
+++ lecture-02/using.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+// Явное включение только std::cout и std::endl в глобальное пространство имён
+using std::cout;
+using std::endl;
+
+namespace A {
+    void hello() { cout << "A::Hello" << endl; }
+
+    namespace B {
+        void hello() { cout << "A::B::Hello" << endl; }
+    };
+};
+
+namespace C {
+    void hello() { cout << "C::Hello" << endl; }
+};
+
+// Включение всего пространства имён A в глобальное пространство имён
+using namespace A;
+
+int main() {
+    A::hello();
+    A::B::hello();
+    C::hello();
+
+    hello();
+    B::hello();
+
+    return 0;
+}
