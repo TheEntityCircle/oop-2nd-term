7a977a6d50abae0fbeaeb2950c67acd6a310d301
Alexey Vasyukov <vasyukov@gmail.com>
2018-02-13 15:27:14 +0300

    Minor update done during the lection

--- 2018/lection02/03_stupid_point.cpp
+++ 2018/lection02/03_stupid_point.cpp
@@ -9,9 +9,13 @@ private:
 	float y;
 
 public:
-	Point2D() { };
+	Point2D() {
+        cout << "Constructor called" << endl;
+    };
 
-	~Point2D() { };
+	~Point2D() {
+        cout << "Destructor called" << endl;
+    };
 
 	void setX(float x) {
 		this->x = x;
@@ -30,13 +34,19 @@ public:
 	}
 };
 
+int func()
+{
+    Point2D p1;
+};
+
 int main()
 {
-	Point2D p;
-	cout << "Point: (" << p.getX() << ", " << p.getY() << ")" << endl;
-	int x = 1;
-	p.setX(x);
-	p.setY(2.5);
-	cout << "Point: (" << p.getX() << ", " << p.getY() << ")" << endl;
-	return 0;
+    Point2D p;
+    cout << "Point: (" << p.getX() << ", " << p.getY() << ")" << endl;
+    int x = 1;
+    func();
+    p.setX(x);
+    p.setY(2.5);
+    cout << "Point: (" << p.getX() << ", " << p.getY() << ")" << endl;
+    return 0;
 }
