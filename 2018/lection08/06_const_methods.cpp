09d10210234a4b212fdf45458e317495486afbe9
Alexey Vasyukov <vasyukov@gmail.com>
2018-03-25 20:07:05 +0300

    Amend: example of const 'propagation'

--- 2018/lection08/06_const_methods.cpp
+++ 2018/lection08/06_const_methods.cpp
@@ -9,13 +9,20 @@ public:
     ClassA(int a) {
         x = a;
     }
+
     int getX() const {
         return x;
     }
+
+    void doNothing() { // должен быть const, но это 'забыли'
+    }
+
     int f1() const {
         //x = 0; // это не будет пропущено, this является const
+        //doNothing(); // и это не будет, что довольно печально
         return x;
     }
+
     int f2() const {
         return x;
     }
