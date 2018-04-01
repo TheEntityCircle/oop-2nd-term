0c385ab9387f3d25b4ffa8385a69b686138dfde4
Alexey Vasyukov <vasyukov@gmail.com>
2018-04-01 18:23:17 +0300

    Year 2018, lection 09

---
+++ 2018/lection09/03_vector_basics.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+const unsigned int N = 5;
+
+int main()
+{
+    // Создаём вектор
+    vector<int> v;
+
+    // Заполняем вектор
+    for (unsigned int i = 0; i < N; i++)
+        v.push_back(i);
+
+    // Выводим содержимое вектора при помощи доступа к элементу по индексу
+    for (unsigned int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+
+    // Добавим ещё данных
+    for (unsigned int i = 0; i < N; i++)
+        v.push_back(i);
+
+    // Изменяем содержимое вектора при помощи доступа к элементу по индексу
+    for (unsigned int i = 0; i < v.size(); i++)
+        v[i] *= 2;
+
+    // Снова выводим на экран
+    for (unsigned int i = 0; i < v.size(); i++)
+        cout << v[i] << " ";
+    cout << endl;
+
+    return 0;
+}
---
+++ 2018/lection09/plot_capacity.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+./vector_capacity 1000 | awk -F '[ ,]' '{ print $2 " " $5 }' > capacity.txt
+gnuplot -p -e "\
+    set grid; \
+    y(x) = x; \
+    set style line 1 lt 1 lc rgb 'red' lw 2; \
+    set style line 2 lt 2 lc rgb 'blue' lw 2 dashtype 3; \
+    plot y(x)  ls 2, 'capacity.txt' ls 1 with l; \
+"
