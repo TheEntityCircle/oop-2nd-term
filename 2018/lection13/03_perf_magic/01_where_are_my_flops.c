7b0189ebfe1ee2e18bb9b74136144edf8a1c6730
Alexey Vasyukov <vasyukov@gmail.com>
2018-04-30 11:53:56 +0300

    Year 2018, lection 13 -- offtop about some performance optimizations

---
+++ 2018/lection13/03_perf_magic/01_where_are_my_flops.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 1000*1000*1000
+
+int main()
+{
+    double* a = (double*)malloc(SIZE*sizeof(double));
+
+    double result = 1;
+    long i;
+    for(i = 0; i < SIZE; i++) {
+        result = result * a[i];
+    }
+
+    printf("Res = %f\n", result);
+    return 0;
+}
