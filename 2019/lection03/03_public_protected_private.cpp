2c11dfec6696951e4f3ba65c6ac780c6fe6a0b00
Alexey Vasyukov <vasyukov@gmail.com>
2019-02-22 05:56:30 +0300

    Year 2019, lection 03

---
+++ 2019/lection03/03_public_protected_private.cpp
@@ -0,0 +1,30 @@
+class A 
+{
+public:
+    int x;
+protected:
+    int y;
+private:
+    int z;
+};
+
+class B : public A
+{
+    // x is public
+    // y is protected
+    // z is not accessible from B
+};
+
+class C : protected A
+{
+    // x is protected
+    // y is protected
+    // z is not accessible from C
+};
+
+class D : private A // default one
+{
+    // x is private
+    // y is private
+    // z is not accessible from D
+};
---
+++ 2019/lection03/04_access_violation.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+private:
+    int secret;
+
+public:
+    A(int i) {
+        this->secret = i;
+    }
+
+    void describe() {
+        cout << "I'm A, my value is: " << secret << endl;
+    }
+};
+
+class B
+{
+public:
+    B() {
+    }
+
+    // Класс B получает указатель на класс A и что-то с ним делает
+    void run(A* a) {
+        // Работаем с классом A, вызываем его публичные методы - это ок
+        cout << "I'm B, I'm going to handle this one: ";
+        a->describe();
+        // Это не скомпилится, так как secret объявлен в классе A как private
+        cout << "Class B run: " << a->secret << endl;
+        a->secret = 13;
+    }
+};
+
+int main()
+{
+    // Создаём экземпляр класса A, пусть он сделает свою работу
+    A a(42);
+    a.describe();
+
+    // Создаём экземпляр класса B
+    B b;
+    // Пробуем передать ему указатель на A для обработки
+    b.run(&a);
+    // Проверяем, как дела у A после обработки
+    a.describe();
+
+    return 0;
+}
