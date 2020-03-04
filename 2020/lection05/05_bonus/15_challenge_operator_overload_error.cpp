3375e9c1658177dbee7b06ca9923983e702d60c7
Alexey Vasyukov <vasyukov@gmail.com>
2020-03-04 03:34:22 +0300

    2020, lection 05

---
+++ 2020/lection05/05_bonus/15_challenge_operator_overload_error.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Данный пример не работает, падая с Segmentation fault.
+// Попробуйте найти ошибку.
+
+class Person
+{
+protected:
+    string name;
+public:
+    Person(string _name) : name(_name) { }
+    ~Person() { }
+    string getName() const { return name; }
+};
+
+std::ostream& operator<<(std::ostream& os, const Person& p) {
+    os << "(" << p.getName() << ")";
+}
+
+int main()
+{
+    Person p("Alice");
+    cout << p << endl;
+    return 0;
+}
