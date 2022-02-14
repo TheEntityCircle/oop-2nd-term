94f1e469aab6d726ed96887dd9add25734b93f12
Alexey Vasyukov <vasyukov@gmail.com>
2022-02-14 22:20:17 +0300

    2022, lection 03

---
+++ 2022/lection03/04_multiple_inheritance/01_multiple_interfaces.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+// Интерфейс «некий зверь», с которым работает движок
+class Animal {
+public:
+    virtual void move() = 0;
+    virtual ~Animal() {};
+};
+
+// Интерфейс «некое изображение», с которым работает визуализатор
+class Drawing {
+public:
+    virtual void show() = 0;
+    virtual ~Drawing() {};
+};
+
+// Реализовать несколько интерфейсов - предельно штатная ситуация.
+class Snake : public Animal, public Drawing {
+public:
+    void move();
+    void show();
+};
+
+void Snake::move()
+{
+    cout << "... snake moves ..." << endl;
+}
+
+void Snake::show()
+{
+    cout << "... drawing snake ..." << endl;
+}
+
+int main()
+{
+    // Так со змейкой общается движок
+    Animal* a = new Snake();
+    a->move();
+    delete a;
+
+    // А так - визуализатор
+    Drawing* d = new Snake();
+    d->show();
+    delete d;
+    return 0;
+}
